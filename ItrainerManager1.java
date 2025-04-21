699978317a6a5e868b39596ca719596b079a0f42
Coding-Cookie25 <aanshikamadan@gmail.com>
2025-04-22 00:33:58 +0530

    Update ItrainerManager1.java

--- ItrainerManager1.java
+++ ItrainerManager1.java
@@ -4,10 +4,6 @@ import model.Trainer;
 import java.util.List;
 
 public interface ItrainerManager1 {
-    boolean addTrainer(Trainer trainer);
+    void addTrainer(Trainer trainer);
     List<Trainer> getAllTrainers();
-    Trainer getTrainerById(int id);
-    boolean removeTrainer(int id);
-    boolean updateTrainer(Trainer trainer);
-    int getTrainerCount();
 }
